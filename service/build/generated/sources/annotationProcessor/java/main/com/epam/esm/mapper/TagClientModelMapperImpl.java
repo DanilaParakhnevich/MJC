40b035b9602f492f493ffc99f3ec62db4ddd0034
Dendil <beylon228822@gmail.com>
2022-01-29 20:45:42 +0300

    service done, controller remain

---
+++ service/build/generated/sources/annotationProcessor/java/main/com/epam/esm/mapper/TagClientModelMapperImpl.java
@@ -0,0 +1,27 @@
+package com.epam.esm.mapper;
+
+import com.epam.esm.dto.TagClientModel;
+import com.epam.esm.entity.TagEntity;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-01-29T19:51:45+0300",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.1.jar, environment: Java 1.8.0_301 (Oracle Corporation)"
+)
+public class TagClientModelMapperImpl implements TagClientModelMapper {
+
+    @Override
+    public TagClientModel tagToTagClientModel(TagEntity tag) {
+        if ( tag == null ) {
+            return null;
+        }
+
+        TagClientModel tagClientModel = new TagClientModel();
+
+        tagClientModel.setId( tag.getId() );
+        tagClientModel.setName( tag.getName() );
+
+        return tagClientModel;
+    }
+}
